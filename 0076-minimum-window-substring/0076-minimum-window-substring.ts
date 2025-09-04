00b3c20542eb9132bc59f6ac7eda34e3d5f1b67c
natnaelAbebaw <129087296+natnaelAbebaw@users.noreply.github.com>
2025-09-04 16:34:34 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.ts
@@ -0,0 +1,48 @@
+function minWindow(s: string, t: string): string {
+ const tmap = {};
+ const smap = {};
+ let minSubStr = s;
+ let l = 0;
+
+ for(let char of t){
+    tmap[char] = (tmap[char] || 0) + 1
+ }
+
+ function allTIsInS(sm, tm){
+    for(let key in tm){
+        if((sm[key] || 0) < tm[key])
+        return false;
+    }
+    return true;
+ }
+
+ for(let r = 0; r < s.length; r++){
+    const char = s[r];
+
+    if(char in tmap){
+    smap[char] = (smap[char] || 0) + 1;
+    }
+    while(allTIsInS(smap,tmap)){
+        const newSubStr = s.slice(l, r + 1);
+        minSubStr = minSubStr.length >  newSubStr.length ? newSubStr : minSubStr;
+        const char = s[l];
+        if(char in tmap){
+            smap[char] = (smap[char] || 0) - 1;
+        }
+        l++
+    }
+    
+
+ }
+
+if(minSubStr.length < t.length){
+   minSubStr = ""; 
+}
+
+if(minSubStr.length == t.length && minSubStr != t){
+   minSubStr = ""
+}
+
+return minSubStr;
+
+};
