1cfd5144d4504ba1273ebb52efb87b24f01a5311
natnaelAbebaw <129087296+natnaelAbebaw@users.noreply.github.com>
2025-09-27 16:59:00 +0300

    Time: 0 ms (100%), Space: 61.5 MB (19.96%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.ts
@@ -0,0 +1,15 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function moveZeroes(nums: number[]): void {
+    let zeros = 0;
+    for(let i = 0; i<nums.length; i++){
+      if(nums[i]==0){
+        zeros++
+      }else{
+        if(!zeros) continue;
+        nums[i-zeros] = nums[i];
+        nums[i] = 0;
+      }
+    }
+};
